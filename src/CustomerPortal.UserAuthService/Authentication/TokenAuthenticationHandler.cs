4c1fa7099ed9cdd207ee96fd55c337c5b60a0a0e
Alexander Menze <mail@alexandermenze.de>
2025-04-12 09:34:33 +0200

    Finish implementation

--- src/CustomerPortal.UserAuthService/Authentication/TokenAuthenticationHandler.cs
+++ src/CustomerPortal.UserAuthService/Authentication/TokenAuthenticationHandler.cs
@@ -1,33 +1,68 @@
+using System.Security.Claims;
+using System.Text;
 using System.Text.Encodings.Web;
+using CustomerPortal.UserAuthService.Domain.Aggregates;
+using CustomerPortal.UserAuthService.Domain.Repositories;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.Extensions.Options;
 using Microsoft.Net.Http.Headers;
 
 namespace CustomerPortal.UserAuthService.Authentication;
 
-public class TokenAuthenticationHandler : AuthenticationHandler<TokenAuthSchemeOptions>
+public class TokenAuthenticationHandler(
+    IOptionsMonitor<TokenAuthSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    IUserRepository userRepository
+) : AuthenticationHandler<TokenAuthSchemeOptions>(options, logger, encoder)
 {
-    public TokenAuthenticationHandler(
-        IOptionsMonitor<TokenAuthSchemeOptions> options,
-        ILoggerFactory logger,
-        UrlEncoder encoder
-    )
-        : base(options, logger, encoder) { }
-
-    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
-        if (!Request.Headers.ContainsKey(HeaderNames.Authorization))
-            return Task.FromResult(AuthenticateResult.NoResult());
+        if (Request.Headers.TryGetValue(HeaderNames.Authorization, out var value) is false)
+            return AuthenticateResult.NoResult();
 
-        var authHeader = Request.Headers[HeaderNames.Authorization].ToString();
+        var authHeader = value.ToString();
 
         if (authHeader.StartsWith("Bearer ") is false)
-            return Task.FromResult(AuthenticateResult.NoResult());
+            return AuthenticateResult.NoResult();
 
-        var token = authHeader["Bearer ".Length..];
+        var userTokenBase64 = authHeader["Bearer ".Length..];
+        var userToken = Encoding.UTF8.GetString(Convert.FromBase64String(userTokenBase64));
+        var split = userToken.Split(':');
 
-        // Token should contain the userId and the token, probably. Need to think of a concept for this.
+        if (split.Length != 2)
+            return AuthenticateResult.NoResult();
 
-        throw new NotImplementedException();
+        if (Guid.TryParse(split[0], out var userId) is false)
+            return AuthenticateResult.NoResult();
+
+        var token = split[1];
+
+        var user = await userRepository.GetById(userId);
+
+        if (user is null)
+            return AuthenticateResult.Fail("Invalid token");
+
+        if (user.IsSessionValidAt(token, DateTime.UtcNow) is false)
+            return AuthenticateResult.Fail("Invalid token");
+
+        return AuthenticateResult.Success(
+            new AuthenticationTicket(CreateFromUser(user), Scheme.Name)
+        );
+    }
+
+    private static ClaimsPrincipal CreateFromUser(User user)
+    {
+        var claimsIdentity = new ClaimsIdentity(
+            [
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+                new Claim(ClaimTypes.Role, user.Role.ToString()),
+            ]
+        );
+
+        return new ClaimsPrincipal(claimsIdentity);
     }
 }
