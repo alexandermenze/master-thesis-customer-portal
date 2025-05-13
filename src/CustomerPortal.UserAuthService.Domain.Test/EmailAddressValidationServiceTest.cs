e5bf94ca77caf98a13087e386c60f77902d10245
Alexander Menze <mail@alexandermenze.de>
2025-05-13 21:19:01 +0200

    Add unit tests

---
+++ src/CustomerPortal.UserAuthService.Domain.Test/EmailAddressValidationServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using CustomerPortal.UserAuthService.Domain.Exceptions;
+using CustomerPortal.UserAuthService.Domain.Services;
+
+namespace CustomerPortal.UserAuthService.Domain.Test;
+
+[TestClass]
+public sealed class EmailAddressValidationServiceTest
+{
+    private readonly EmailAddressValidationService _service = new();
+
+    [TestMethod]
+    public void Email_WithoutAt_IsInvalid()
+    {
+        Assert.ThrowsException<DomainValidationException>(() => _service.EnsureIsValid("mail.de"));
+    }
+
+    [TestMethod]
+    public void Email_Normal_IsValid()
+    {
+        _service.EnsureIsValid("mail@nothing.de");
+    }
+}
